d793cf953800f9ae85a5dcdbdde1a1e87aaf3fa0
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-01-07 16:08:46 +0900

    Implemented some code: LINQ Query Syntax

---
+++ ACM.BL.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        //TODO
+
+        [TestMethod]
+        public void FindTestExistingCustomer()
+        {
+            // Arrange
+            var repository = new CustomerRepository();
+            var customerList = repository.Retrieve();
+
+            // Act
+            var result = repository.Find(customerList, 2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.CustomerId);
+            Assert.AreEqual("Baggins", result.LastName);
+            Assert.AreEqual("Bilbo", result.FirstName);
+        }
+
+        [TestMethod]
+        public void FindTestNotFound()
+        {
+            // Arrange
+            var repository = new CustomerRepository();
+            var customerList = repository.Retrieve();
+
+            // Act
+            var result = repository.Find(customerList, 42);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ ACM.BL/CustomerRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        public Customer Find(List<Customer> customerList, int customerId)
+        {
+            Customer foundCustomer = null;
+
+            foreach (var c in customerList)
+            {
+                if (c.CustomerId == customerId)
+                {
+                    foundCustomer = c;
+                    break;
+                }
+            }
+
+            return foundCustomer;
+        }
+
+        public List<Customer> Retrieve()
+        {
+            var custList = new List<Customer>
+            {
+                new Customer
+                {
+                    CustomerId = 1,
+                    FirstName = "Frodo",
+                    LastName = "Baggins",
+                    EmailAddress = "fb@hob.me",
+                    CustomerTypeId = 1,
+                },
+                new Customer
+                {
+                    CustomerId = 2,
+                    FirstName = "Bilbo",
+                    LastName = "Baggins",
+                    EmailAddress = "bb@hob.me",
+                    CustomerTypeId = null
+                },
+                new Customer
+                {
+                    CustomerId = 3,
+                    FirstName = "Samwise",
+                    LastName = "Gamgee",
+                    EmailAddress = "sg@hob.me",
+                    CustomerTypeId = 1
+                },
+                new Customer
+                {
+                    CustomerId = 4,
+                    FirstName = "Rosie",
+                    LastName = "Cotton",
+                    EmailAddress = "rc@hob.me",
+                    CustomerTypeId = 2
+                }
+            };
+            return custList;
+        }
+    }
+}
