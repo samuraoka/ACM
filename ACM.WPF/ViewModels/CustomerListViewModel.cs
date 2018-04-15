f01e4b694ad6edccdd06871e55f055fa812157c8
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-04-15 12:22:32 +0900

    Implement some code to display customer data.

---
+++ ACM.WPF/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ACM.BL;
+using ACM.Data;
+using System.Collections.ObjectModel;
+
+namespace ACM.WPF.ViewModels
+{
+    public class CustomerListViewModel : ViewModelBase
+    {
+        private ObservableCollection<Customer> customers;
+
+        public ObservableCollection<Customer> Customers
+        {
+            get { return customers; }
+            set
+            {
+                if (customers != value)
+                {
+                    customers = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        CustomerRepository customerRepository = new ACMCustomerRepository();
+
+        public CustomerListViewModel()
+        {
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            customers = new ObservableCollection<Customer>(
+                customerRepository.Retrieve());
+
+        }
+    }
+}
